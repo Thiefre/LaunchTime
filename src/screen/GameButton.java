2006b929fc88f25c6fd325e8ffdac5e0aa094e89
Thiefre <kevinvluu@gmail.com>
2019-04-10 22:38:40 -0700

    Added icons

--- src/screen/GameButton.java
+++ src/screen/GameButton.java
@@ -1,7 +1,11 @@
 package screen;
 
 import java.awt.Color;
+import java.io.File;
+
 import javax.swing.JButton;
+import javax.swing.filechooser.FileSystemView;
+
 import data.Game;
 
 public class GameButton extends JButton
@@ -15,6 +19,7 @@ public class GameButton extends JButton
 	{
 		this.g = g;
 		setText(g.name);
+		setIcon(FileSystemView.getFileSystemView().getSystemIcon(new File(g.path)));
 		setBackground(new Color(75, 80, 95));
 		setForeground(Color.WHITE);
 	}
